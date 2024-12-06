97a821f56a4c3840894b1035074098b6f0a0f635
AkagiCode <yuya.akagi2@gmail.com>
2024-12-06 10:36:03 +0900

    commit!

--- resources/user.py
+++ resources/user.py
@@ -1,29 +1,42 @@
+import os
+import requests
+from flask import current_app
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from passlib.hash import pbkdf2_sha256
 from sqlalchemy.exc import SQLAlchemyError
 from flask_jwt_extended import create_access_token, create_refresh_token, get_jwt_identity, jwt_required, get_jwt
+from sqlalchemy import or_
 
 from db import db
 from blocklist import BLOCKLIST
 from models import UserModel
-from schemas import UserSchema
+from schemas import UserSchema, UserRegisterSchema
+from task import send_user_registration_email
 
 blp = Blueprint("Users", "users", description="Operations on users")
 
 @blp.route("/register")
 class UserRegister(MethodView):
-    @blp.arguments(UserSchema)
+    @blp.arguments(UserRegisterSchema)
     def post(self, user_data):
-        if UserModel.query.filter(UserModel.username == user_data["username"]).first():
-            abort(409,message="A user with that name already exists.")
+        if UserModel.query.filter(
+            or_(
+                UserModel.username == user_data["username"],
+                UserModel.email == user_data["email"]
+            )
+        ).first():
+            abort(409,message="A user with that name or email already exists.")
         user = UserModel(
             username = user_data["username"],
+            email = user_data["email"],
             password = pbkdf2_sha256.hash(user_data["password"])
         )
         
         db.session.add(user)
         db.session.commit()
+        
+        current_app.queue.enqueue(send_user_registration_email, user.email, user.username)
         
         return {"message": "User created successfully."}, 201
 
